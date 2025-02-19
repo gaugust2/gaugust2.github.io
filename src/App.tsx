decf5a12afe8a7427d66c2663ad1f29ff2c69666
gaugust2 <gaugust2@uwo.ca>
2025-02-18 23:41:39 -0500

    prettier sidebar

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,28 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+//import { useState } from 'react'
 import './App.css'
 import Sidebar from './components/Sidebar'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const sections = [
+    {
+      href: "#About",
+      name: "About"
+    },
+    {
+      href: "#Experience",
+      name: "Experience"
+    },
+    {
+      href: "#Skills",
+      name: "Skills"
+    }
+  ]
 
   return (
     <div className='font-sans'>
       <div className='flex h-screen'>
-        <Sidebar/>
-        <div className='ml-16 flex-1 p-6'>
+        <Sidebar sections={sections} />
+        <div className='ml-50 flex-1 p-6'>
           Content
           <section id='About'>
           </section>
@@ -26,29 +37,3 @@ function App() {
 }
 
 export default App
-
-
-/*
-
-<div>
-          <a href="https://vite.dev" target="_blank">
-            <img src={viteLogo} className="logo" alt="Vite logo" />
-          </a>
-          <a href="https://react.dev" target="_blank">
-            <img src={reactLogo} className="logo react" alt="React logo" />
-          </a>
-        </div>
-        <h1>Vite + React</h1>
-        <div className="card">
-          <button onClick={() => setCount((count) => count + 1)}>
-            count is {count}
-          </button>
-          <p>
-            Edit <code>src/App.tsx</code> and save to test HMR
-          </p>
-        </div>
-        <p className="read-the-docs">
-          Click on the Vite and React logos to learn more
-        </p>
-
-*/
--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -1,16 +1,19 @@
-function Sidebar() {
+interface SidebarProps {
+  sections: {
+    href: string,
+    name: string
+  }[]
+}
+
+function Sidebar({ sections }: SidebarProps) {
   return (
-    <div className='group flex flex-col items-start text-black w-30 hover:w-60 transition-all duration-300 h-1/2 top-1/4 fixed'>
-      <ul className='p-4 text-xl'>
-        <li className='p-1 mb-4 '>
-          <a className="underline-hover" href='#About' data-nav-section='About'>About</a>
-        </li>
-        <li className='p-1 mb-4'>
-          <a className="underline-hover" href='#Experience' data-nav-section='Experience'>Experience</a>
-        </li>
-        <li className='p-1 mb-4'>
-          <a className="underline-hover" href='#Skills' data-nav-section='Skills'>Skills</a>
-        </li>
+    <div className='group flex flex-col items-start text-black w-600 top-1/4 fixed'>
+      <ul className={`p-4 text-xl`}>
+        {sections.map((section) => (
+          <li className='p-1 mb-4'>
+            <a className={`underline-hover sidebar-heading-hover`} href={section.href}>{section.name}</a>
+          </li>
+        ))}
       </ul>
     </div>
   )
